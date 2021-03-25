1e1a743049c78ab04b583ba963f1115fb7bf07cd
yuuki higaki <yuukihigaki@hgaiji.local>
2021-03-26 08:44:34 +0900

    issue #5 : hotpepparのrequest関数のリファクタリング

--- app/model/hotpepper.py
+++ app/model/hotpepper.py
@@ -7,12 +7,12 @@ class HotPepper:
         self.api_url = settings.HOT_PEPPAR_API_URL
 
     def gourmet(self, params):
-        return self.__request_of(params, "gourmet")
+        return self.__request_of("gourmet", params)
 
     def genre_master(self, params):
-        return self.__request_of(params, "genre")
+        return self.__request_of("genre", params)
 
-    def __request_of(self, params, api_type="genre"):
+    def __request_of(self, api_type="genre",params=None):
         self.api_url = self.api_url.replace("__API_TYPE__", api_type)
         response = requests.get(self.api_url, params)
         return response.json()

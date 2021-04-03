0700db2a284548dd7cd545405340cd34c7b8272a
yuuki higaki <yuukihigaki@hgaiji.local>
2021-04-03 18:58:33 +0900

    firebase firestore document name change

--- app/controller/restaurant.py
+++ app/controller/restaurant.py
@@ -7,15 +7,13 @@ from app.model.restaurant import RestaurantModel
 class Restaurant:
     def __init__(self):
         self.firebase = Firebase()
-        self.fb_collection = "testCollection"
-        self.fb_document = "testDocument"
         
     def add_favorite(self, restaurants, user_id):
         document = sys._getframe().f_code.co_name.split("_")[1] + self.__class__.__name__
         for k, v in restaurants.items():
             self.firebase.upsert_db_data_to(
                 collection= user_id,
-                document=self.fb_document,
+                document=document,
                 data=self.firebase._array_data_adding_format(k, v)
             )
     
@@ -24,16 +22,17 @@ class Restaurant:
         document = sys._getframe().f_code.co_name.split("_")[1] + self.__class__.__name__
         fav_data = self.firebase.get_db_data(
             collection=user_id,
-            document=self.fb_document,
+            document=document,
         )
-        return RestaurantModel(ids = list(fav_data.values())[0])
+
+        return RestaurantModel(ids = fav_data.get("ids",[]))
 
     def delete_favorite(self, restaurants, user_id):
         document = sys._getframe().f_code.co_name.split("_")[1] + self.__class__.__name__
         for k, v in restaurants.items():
             self.firebase.delete_db_dataget_db_data(
                 collection=user_id,
-                document=self.fb_document,
+                document=document,
                 data=self.firebase._array_data_deleting_format(k, v)
             )
 
